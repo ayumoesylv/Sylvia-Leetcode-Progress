c29e9e1a505354eaaf1f723d20a069f1ded1fd52
Sylvia Zhang <83683923+ayumoesylv@users.noreply.github.com>
2026-07-15 20:06:45 +0000

    Sync LeetCode submission - Valid Anagram (python3)

---
+++ problems/valid_anagram/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_s = {}
+        dict_t = {}
+        for char in s: 
+            if not char in dict_s:
+                dict_s[char] = 1
+            else: 
+                dict_s[char] += 1
+        for char in t:
+            if not char in dict_t:
+                dict_t[char] = 1
+            else: 
+                dict_t[char] += 1
+        return dict_s == dict_t
